9e3f0f24bd9674a37807ee3a6b7dea1bfc6e4bdd
Rushikesh <rushikeshingole467@gmail.com>
2025-04-07 09:55:05 +0530

    Update code after removing style.css file

--- CheckEmailPage.jsx
+++ CheckEmailPage.jsx
@@ -1,5 +1,4 @@
 import { Link, useLocation } from "react-router-dom";
-import "./Style.css";
 import { useRef } from "react";
 
 const CheckEmailPage = () => {
@@ -24,39 +23,44 @@ const CheckEmailPage = () => {
   };
 
   return (
-    <div className="w-[400px] mx-auto my-[10rem] p-[50px] bg-white rounded-[10px] shadow-[0_0_15px_rgba(0,0,0,0.1)]">
-      <h2 className="text-[30px] font-medium leading-[100%] tracking-[0%] text-center mb-6 font-jost pb-[20px]">
-        Check Your Email
-      </h2>
+    <div
+      style={{ backgroundColor: "#edf2f7" }}
+      className="min-h-screen flex items-center justify-center"
+    >
+      <div className="w-[400px] mx-auto my-[10rem] p-[50px] bg-white rounded-[10px] shadow-[0_0_15px_rgba(0,0,0,0.1)]">
+        <h2 className="text-[30px] font-medium leading-[100%] tracking-[0%] text-center mb-6 font-jost pb-[20px]">
+          Check Your Email
+        </h2>
 
-      <p className=" pb-[30px] text-[12px] leading-[20px] align-middle font-normal font-jost">
-        Code sent to <strong>{email}</strong>. Please enter the code below
-      </p>
+        <p className=" pb-[30px] text-[12px] leading-[20px] align-middle font-normal font-jost">
+          Code sent to <strong>{email}</strong>. Please enter the code below
+        </p>
 
-      <div className="flex justify-center gap-2 pt-2 pb-2 pl-12 pr-12 ">
-        {[0, 1, 2, 3].map((i) => (
-          <input
-            key={i}
-            type="text"
-            maxLength="1"
-            onChange={(e) => handleChange(e, i)}
-            onKeyDown={(e) => handleKeyDown(e, i)}
-            ref={(el) => (inputsRef.current[i] = el)}
-            className="w-10 h-10 text-center text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-[30px]"
-          />
-        ))}
+        <div className="flex justify-center gap-2 pt-2 pb-2 pl-12 pr-12 ">
+          {[0, 1, 2, 3].map((i) => (
+            <input
+              key={i}
+              type="text"
+              maxLength="1"
+              onChange={(e) => handleChange(e, i)}
+              onKeyDown={(e) => handleKeyDown(e, i)}
+              ref={(el) => (inputsRef.current[i] = el)}
+              className="w-10 h-10 text-center text-lg border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-[30px]"
+            />
+          ))}
+        </div>
+        {/* When on create new password button redirect to the next page name create new passowrd */}
+        <Link to="newPasswordPage">
+          <button className="w-full px-4 py-2 bg-[#2873f0] text-white text-base rounded-md hover:bg-[#0059f4] cursor-pointer mb-[15px] text-[12px]">
+            Create a new Password
+          </button>
+        </Link>
+        <p className=" pb-[30px] text-[12px] leading-[20px] align-middle font-normal font-jost">
+          Didn't receive code ?
+          <strong className="underline">Resend Code</strong> <br></br>
+          Resend Code in 00.59
+        </p>
       </div>
-      {/* When on create new password button redirect to the next page name create new passowrd */}
-      <Link to="newPasswordPage">
-        <button className="w-full px-4 py-2 bg-[#2873f0] text-white text-base rounded-md hover:bg-[#0059f4] cursor-pointer mb-[15px] text-[12px]">
-          Create a new Password
-        </button>
-      </Link>
-      <p className=" pb-[30px] text-[12px] leading-[20px] align-middle font-normal font-jost">
-        Didn't receive code ?<strong className="underline">Resend Code</strong>{" "}
-        <br></br>
-        Resend Code in 00.59
-      </p>
     </div>
   );
 };
--- LoginPage.jsx
+++ LoginPage.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import "./Style.css";
 import { useState } from "react";
 
 const LoginPage = () => {
@@ -10,42 +9,48 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="w-[400px] mx-auto my-[10rem] p-[50px] bg-white rounded-[10px] shadow-[0_0_15px_rgba(0,0,0,0.1)]">
-      <h2 className="text-[30px] font-medium leading-[100%] tracking-[0%] text-center mb-6 font-jost pb-[20px] ">
-        Admin Login
-      </h2>
+    <div
+      style={{ backgroundColor: "#edf2f7" }}
+      className="min-h-screen flex items-center justify-center"
+    >
+      <div className="w-[400px] mx-auto my-[10rem] p-[50px] bg-white rounded-[10px] shadow-[0_0_15px_rgba(0,0,0,0.1)]">
+        <h2 className="text-[30px] font-medium leading-[100%] tracking-[0%] text-center mb-6 font-jost pb-[20px] ">
+          Admin Login
+        </h2>
 
-      <input
-        type="email"
-        placeholder="Enter Your Email Address"
-        className="w-full mb-[15px] px-[12px] py-[10px] rounded-md text-[14px] bg-[#f2f3f5] outline-none mb-[40px]"
-      />
-
-      <div className="relative">
         <input
-          style={{ WebkitTextSecurity: showPassword ? "none" : "disc" }}
-          placeholder="Password"
-          className="w-full mb-[15px] px-[12px] py-[10px] rounded-md text-[14px] bg-[#f2f3f5] outline-none mb-[40px]"
+          type="email"
+          placeholder="Enter Your Email Address"
+          className="w-full px-[12px] py-[10px] rounded-md text-[14px] bg-[#f2f3f5] outline-none mb-[40px]"
         />
-        <span
-          onClick={togglePasswordVisibility}
-          className="absolute right-[10px] top-[10%] translate-y-[10%] text-[#888] cursor-pointer"
-        >
-          {showPassword ? (
-            <i className="fa-solid fa-eye-slash"></i>
-          ) : (
-            <i className="fa-solid fa-eye"></i>
-          )}
-        </span>
+
+        <div className="relative">
+          <input
+            style={{ WebkitTextSecurity: showPassword ? "none" : "disc" }}
+            placeholder="Password"
+            className="w-full  px-[12px] py-[10px] rounded-md text-[14px] bg-[#f2f3f5] outline-none mb-[40px]"
+          />
+          <span
+            onClick={togglePasswordVisibility}
+            className="absolute right-[10px] top-[10%] translate-y-[10%] text-[#888] cursor-pointer"
+          >
+            {showPassword ? (
+              <i className="fa-solid fa-eye-slash"></i>
+            ) : (
+              <i className="fa-solid fa-eye"></i>
+            )}
+          </span>
+        </div>
+
+        <p className="text-right font-medium text-[12px] mb-[40px]">
+          <Link to="/forgot-password">Forget Password?</Link>
+        </p>
+        {/* <Link to="/Home"> */}
+        <button className="w-full px-4 py-2 bg-[#2873f0] text-white text-base rounded-md hover:bg-[#0059f4] cursor-pointer text-[12px]">
+          Login
+        </button>
+        {/* </Link> */}
       </div>
-
-      <p className="text-right font-medium text-[12px] mb-[40px]">
-        <Link to="/forgot-password">Forget Password?</Link>
-      </p>
-
-      <button className="w-full px-4 py-2 bg-[#2873f0] text-white text-base rounded-md hover:bg-[#0059f4] cursor-pointer text-[12px]">
-        Login
-      </button>
     </div>
   );
 };
