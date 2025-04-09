ff4abc7f74e872f9cbd7b1dad6ebc3ef22fdcb6b
Rushikesh <rushikeshingole467@gmail.com>
2025-04-09 17:29:07 +0530

    changes in routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import Dashboard from "./Pages/Dashboard";
 import Profile from "./Pages/Profile";
 import Product from "./Pages/Product";
 import Chart from "./Pages/Chart";
-import ResetPassword from "./Pages/ResetPassword";
 import PasswordSuccess from "./Pages/PasswordSuccess";
 import Setting from "./Pages/Setting";
 import Contact from "./Pages/Contact";
+import ResetPassword from "./Pages/ResetPassword";
 
 function App() {
   return (
@@ -19,13 +19,13 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
         <Route path="/CheckEmail" element={<CheckEmail />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
+        <Route path="/PasswordSuccess" element={<PasswordSuccess />} />
         <Route element={<Layout />}>
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Product" element={<Product />} />
           <Route path="/Chart" element={<Chart />} />
-          <Route path="/ResetPassword" element={<ResetPassword />} />
-          <Route path="/PasswordSuccess" element={<PasswordSuccess />} />
           <Route path="/Setting" element={<Setting />} />
           <Route path="/Contact" element={<Contact />} />
         </Route>
