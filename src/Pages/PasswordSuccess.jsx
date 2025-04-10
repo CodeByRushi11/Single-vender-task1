669a40379239100d7fdf025e10e9981ca16ed583
ayushharode01 <131837936+ayushharode01@users.noreply.github.com>
2025-04-10 15:04:42 +0530

    Update PasswordSuccess.jsx

--- src/Pages/PasswordSuccess.jsx
+++ src/Pages/PasswordSuccess.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-
-
+import { Link } from "react-router-dom";
+import success from "../assets/success.png";
 const PasswordSuccess = () => {
-  
-
- 
-
   return (
     <div className="min-h-screen bg-[#eaf3f7] flex items-center justify-center px-4">
       <div className="bg-white rounded-lg shadow-md p-8 max-w-md w-full text-center">
@@ -14,7 +10,7 @@ const PasswordSuccess = () => {
         </h2>
 
         <img
-          src="/successillu.jpg"
+          src={success}
           alt="Success"
           className="w-40 h-40 mx-auto mb-4"
         />
@@ -23,13 +19,11 @@ const PasswordSuccess = () => {
           Your password has been changed successfully. <br />
           You can now use your new password to login to your account.
         </p>
-
-        <button
-         
-          className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-md font-medium transition"
-        >
-          Login
-        </button>
+        <Link to="/Dashboard">
+          <button className="w-full px-4 py-2 bg-[#2873f0] text-white text-base rounded-md hover:bg-[#0059f4] cursor-pointer text-[12px]">
+            Login
+          </button>
+        </Link>
       </div>
     </div>
   );
